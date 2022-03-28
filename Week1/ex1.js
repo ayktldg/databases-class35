e25e220f22f5d77952f4817f3af285b456c64fd6
ayktldg <aykutuludag26@gmail.com>
2022-03-29 01:24:27 +0200

    update functions by DRY principle

--- Week1/ex1.js
+++ Week1/ex1.js
@@ -7,96 +7,69 @@ const connection = mysql.createConnection({
   database: "meetup",
 });
 
+const DROP_MEETUP_DATABASE = "DROP DATABASE IF EXISTS meetup;";
+
+const CREATE_MEETUP_DATABASE = "CREATE DATABASE meetup;";
+
+const USE_MEETUP_DATABASE = "USE meetup;";
+
+const CREATE_INVITEE_TABLE = `CREATE TABLE Invitee(
+  invitee_no INT, invitee_name VARCHAR(100), invited_by VARCHAR(100)
+);`;
+
+const CREATE_MEETING_TABLE = `CREATE TABLE Meeting(
+  meeting_no INT, meeting_title VARCHAR(100), starting_time DATETIME, ending_time DATETIME, room_no INT
+);`;
+
+const CREATE_ROOM_TABLE = `CREATE TABLE Room(
+  room_no INT, room_name VARCHAR(100), floor_number INT
+);`;
+
+const INSERT_INTO_INVITEE = `INSERT INTO Invitee (invitee_no, invitee_name, invited_by) 
+VALUES 
+  (1, 'Fred', 'Sara'),
+  (2, 'Martin', 'Andrew'),
+  (3, 'Ashley', 'Bob'),
+  (4, 'Jane', 'David'),
+  (5, 'John', 'Sara');`;
+
+const INSERT_INTO_MEETING = `INSERT INTO Meeting (meeting_no, meeting_title, starting_time, ending_time, room_no)
+VALUES 
+  (1, 'Intro Js', '2022-04-12 09:30:00', '2022-04-12 15:30:00', 110),
+  (2, 'Databases', '2022-04-15 09:00:00', '2022-04-13 16:30:00', 111),
+  (3, 'Closures', '2022-05-12 11:45:00', '2022-05-12 14:00:00', 112),
+  (4, 'Advanced Js', '2022-06-01 09:00:00', '2022-06-01 17:00:00', 113),
+  (5, 'Intro Js', '2022-07-10 09:30:00', '2022-07-10 15:45:00', 114);`;
+
+const INSERT_INTO_ROOM = `INSERT INTO Room (room_no, room_name, floor_number) 
+VALUES 
+  (101, 'Ocean', 1),
+  (201, 'Garden', 2),
+  (310, 'Pool', 3),
+  (103, 'Sea', 1),
+  (204, 'Forest', 2);`;
+
+const execQuery = (query, message = "Success") => {
+  connection.query(query, (error, results) => {
+    if (error) {
+      throw error;
+    } else {
+      console.log(message);
+      console.log(results);
+    }
+  });
+};
+
 connection.connect();
 
-connection.query("DROP DATABASE IF EXISTS meetup", (error, results) => {
-  if (error) throw error;
-  console.log("meetup database has been dropped");
-});
-
-connection.query("CREATE DATABASE meetup", (error, results) => {
-  if (error) throw error;
-  console.log("meetup database has been created");
-});
-
-connection.query("USE meetup", (error, results) => {
-  if (error) throw error;
-  console.log("Using meetup database");
-});
-
-connection.query(
-  `CREATE TABLE Invitee(
-    invitee_no INT, invitee_name VARCHAR(100), invited_by VARCHAR(100)
-  )`,
-  function (error, results) {
-    if (error) throw error;
-    console.log("Invitee table created.");
-    console.log(results);
-  }
-);
-
-connection.query(
-  `CREATE TABLE Meeting(
-      meeting_no INT, meeting_title VARCHAR(100), starting_time DATETIME, ending_time DATETIME, room_no INT
-    )`,
-  (error, results) => {
-    if (error) throw error;
-    console.log("Meeting table created.");
-    console.log(results);
-  }
-);
-
-connection.query(
-  `CREATE TABLE Room(
-      room_no INT, room_name VARCHAR(100), floor_number INT
-    )`,
-  (error, results) => {
-    if (error) throw error;
-    console.log("Room table created.");
-    console.log(results);
-  }
-);
-
-connection.query(
-  `INSERT INTO Invitee (invitee_no, invitee_name, invited_by) 
-    VALUES 
-      (1, 'Fred', 'Sara'),
-      (2, 'Martin', 'Andrew'),
-      (3, 'Ashley', 'Bob'),
-      (4, 'Jane', 'David'),
-      (5, 'John', 'Sara')`,
-  (error, results) => {
-    if (error) throw error;
-    console.log(results);
-  }
-);
-
-connection.query(
-  `INSERT INTO Room (room_no, room_name, floor_number) 
-      VALUES 
-        (101, 'Ocean', 1),
-        (201, 'Garden', 2),
-        (310, 'Pool', 3),
-        (103, 'Sea', 1),
-        (204, 'Forest', 2)`,
-  (error, results) => {
-    if (error) throw error;
-    console.log(results);
-  }
-);
-
-connection.query(
-  `INSERT INTO Meeting (meeting_no, meeting_title, starting_time, ending_time, room_no)
-    VALUES 
-      (1, 'Intro Js', '2022-04-12 09:30:00', '2022-04-12 15:30:00', 110),
-      (2, 'Databases', '2022-04-15 09:00:00', '2022-04-13 16:30:00', 111),
-      (3, 'Closures', '2022-05-12 11:45:00', '2022-05-12 14:00:00', 112),
-      (4, 'Advanced Js', '2022-06-01 09:00:00', '2022-06-01 17:00:00', 113),
-      (5, 'Intro Js', '2022-07-10 09:30:00', '2022-07-10 15:45:00', 114)`,
-  (error, results) => {
-    if (error) throw error;
-    console.log(results);
-  }
-);
+execQuery(DROP_MEETUP_DATABASE);
+execQuery(CREATE_MEETUP_DATABASE);
+execQuery(USE_MEETUP_DATABASE);
+execQuery(CREATE_INVITEE_TABLE);
+execQuery(CREATE_MEETING_TABLE);
+execQuery(CREATE_ROOM_TABLE);
+execQuery(INSERT_INTO_INVITEE);
+execQuery(INSERT_INTO_MEETING);
+execQuery(INSERT_INTO_ROOM);
 
 connection.end();
--- Week1/ex2.js
+++ Week1/ex2.js
@@ -9,81 +9,25 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
-connection.query(
-  "SELECT Name FROM country WHERE Population > 8000000;",
-  (error, results) => {
-    if (error) throw error;
-    console.log(results);
-  }
-);
+const execQuery = (query) => {
+  connection.query(query, (error, results) => {
+    if (error) {
+      throw error;
+    } else {
+      console.table(results);
+    }
+  });
+};
 
-connection.query(
-  "SELECT Name FROM country WHERE Name LIKE '%land%'",
-  (error, results) => {
-    if (error) throw error;
-    console.log(results);
-  }
-);
-
-connection.query(
-  "SELECT Name FROM city WHERE Population BETWEEN 500000 AND 1000000",
-  (error, results) => {
-    if (error) throw error;
-    console.log(results);
-  }
-);
-
-connection.query(
-  "SELECT Name FROM country WHERE Continent = 'Europe'",
-  (error, results) => {
-    if (error) throw error;
-    console.log(results);
-  }
-);
-
-connection.query(
-  "SELECT Name FROM country ORDER BY SurfaceArea DESC",
-  (error, results) => {
-    if (error) throw error;
-    console.log(results);
-  }
-);
-
-connection.query(
-  "SELECT Name FROM city WHERE CountryCode = 'NLD'",
-  (error, results) => {
-    if (error) throw error;
-    console.log(results);
-  }
-);
-
-connection.query(
-  "SELECT Population FROM city WHERE Name = 'Rotterdam'",
-  (error, results) => {
-    if (error) throw error;
-    console.log(results);
-  }
-);
-
-connection.query(
-  "SELECT * FROM country ORDER BY SurfaceArea DESC LIMIT 10",
-  (error, results) => {
-    if (error) throw error;
-    console.log(results);
-  }
-);
-
-connection.query(
-  "SELECT * FROM city ORDER BY Population DESC LIMIT 10",
-  (error, results) => {
-    if (error) throw error;
-    console.log(results);
-  }
-);
-
-connection.query("SELECT SUM(Population) FROM country", (error, results) => {
-  if (error) throw error;
-  console.log(results);
-});
+execQuery("SELECT Name FROM country WHERE Population > 8000000;");
+execQuery("SELECT Name FROM country WHERE Name LIKE '%land%';");
+execQuery("SELECT Name FROM city WHERE Population BETWEEN 500000 AND 1000000;");
+execQuery("SELECT Name FROM country WHERE Continent = 'Europe';");
+execQuery("SELECT Name FROM country ORDER BY SurfaceArea DESC;");
+execQuery("SELECT Name FROM city WHERE CountryCode = 'NLD';");
+execQuery("SELECT Population FROM city WHERE Name = 'Rotterdam';");
+execQuery("SELECT * FROM country ORDER BY SurfaceArea DESC LIMIT 10;");
+execQuery("SELECT * FROM city ORDER BY Population DESC LIMIT 10;");
+execQuery("SELECT SUM(Population) FROM country;");
 
 connection.end();
